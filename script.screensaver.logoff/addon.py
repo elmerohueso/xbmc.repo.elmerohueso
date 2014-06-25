91eae40f1655b9d9faa158386d6693b0bd1d2d72
elmerohueso <elmerohueso@users.noreply.github.com>
2014-06-25 16:28:40 -0600

    Update addon.py

--- script.screensaver.logoff/addon.py
+++ script.screensaver.logoff/addon.py
@@ -5,8 +5,8 @@ def logoff():
     # Logoff XBMC profile
     xbmc.executebuiltin("System.Logoff()")
 
+    # If running Android, simulate Power button to sleep the device
     if xbmc.getCondVisibility("system.platform.android"):
-        # If running Android, simulate Power button to sleep the device.
         os.system("input keyevent 'KEYCODE_POWER'")
         
 logoff()
