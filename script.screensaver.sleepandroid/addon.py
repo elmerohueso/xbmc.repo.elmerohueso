318901f238ff36f7724bca5505550e59154cc0ee
elmerohueso <elmerohueso@users.noreply.github.com>
2014-06-27 11:52:20 -0600

    Converted all actions to functions
    In case I decide to call them from other addons in the future.

--- script.screensaver.sleepandroid/addon.py
+++ script.screensaver.sleepandroid/addon.py
@@ -6,10 +6,19 @@ __settings__ = xbmcaddon.Addon(id='script.screensaver.sleepandroid')
 __power__ = __settings__.getSetting('powerop')
 __logoff__ = __settings__.getSetting('logoffbool')
 
-if __logoff__ == "true":
+def logoff():
 	xbmc.executebuiltin("System.Logoff()")
+	
+def sleep():
+	os.system("su -c 'input keyevent KEYCODE_POWER'")
+	
+def poweroff():
+	os.system("su -c 'reboot -p'")
+
+if __logoff__ == "true":
+	logoff()
 
 if __power__ == "0":
-	os.system("su -c 'input keyevent KEYCODE_POWER'")
+	sleep()
 elif __power__ == "1":
-	os.system("su -c 'reboot -p'")
+	poweroff()
