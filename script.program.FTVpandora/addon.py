086c3580da69d266265728428ad50e9718243c91
elmerohueso <elmerohueso@users.noreply.github.com>
2014-07-08 11:57:45 -0600

    Display error if app doesn't exist

--- script.program.FTVpandora/addon.py
+++ script.program.FTVpandora/addon.py
@@ -1,6 +1,13 @@
 import xbmc
+import xbmcgui
+
+appname="Pandora"
+packagename="com.pandora.android.gtv"
 
 def launcher():
-        xbmc.executebuiltin('XBMC.StartAndroidActivity("com.pandora.android.gtv")')
+        try:
+                xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename)
+        except:
+                xbmcgui.Dialog().ok('App Not Installed', 'The "%s" app (%s) could not be started as it has not been installed.  Please install the "%s" app and try again.' % (appname, packagename, appname))
 
 launcher()
