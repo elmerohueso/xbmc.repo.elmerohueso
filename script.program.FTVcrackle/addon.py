2ffee23f411504b4be38609a9a2cb6b890ed6593
elmerohueso <elmerohueso@gmail.com>
2014-07-09 21:13:57 -0600

    android shortcuts

---
+++ script.program.FTVcrackle/addon.py
@@ -0,0 +1,15 @@
+import xbmc
+import xbmcgui
+import os
+
+appname="Crackle"
+packagename="com.gotv.crackle.handset"
+
+def launcher():
+	packagepath = os.popen('pm path %s' % packagename).read()
+	if packagepath:
+		xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename)
+	else:
+		xbmcgui.Dialog().ok('App Not Installed', 'This is only a shortcut.  Please install the "%s" app (%s) and try again.' % (appname, packagename))
+
+launcher()
