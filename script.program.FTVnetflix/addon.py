42b6694dddf15c2fa2ec1f937e916548d87e06ff
elmerohueso <elmerohueso@gmail.com>
2014-07-10 20:11:45 -0600

    Many updates

--- script.program.FTVnetflix/addon.py
+++ script.program.FTVnetflix/addon.py
@@ -1,7 +1,15 @@
 import xbmc
+import xbmcgui
+import os
 
-class launcher(xbmc.Monitor):
-        xbmc.executebuiltin('XBMC.StartAndroidActivity("org.chromium.content_shell_apk")')
+appname="Netflix"
+packagename="org.chromium.content_shell_apk"
 
-mylauncher = launcher()
-del mylauncher
+def launcher():
+	packagepath = os.popen('pm path %s' % packagename).read()
+	if packagepath:
+		xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename)
+	else:
+		xbmcgui.Dialog().ok('App Not Installed', 'This is only a shortcut.  Please install the "%s" app (%s) and try again.' % (appname, packagename))
+
+launcher()
--- script.program.KFTVlauncher/addon.py
+++ script.program.KFTVlauncher/addon.py
@@ -1,6 +1,15 @@
 import xbmc
+import xbmcgui
+import os
+
+appname="Fire TV Launcher"
+packagename="com.amazon.tv.launcher"
 
 def launcher():
-        xbmc.executebuiltin('XBMC.StartAndroidActivity("com.amazon.tv.launcher")')
+	packagepath = os.popen('pm path %s' % packagename).read()
+	if packagepath:
+		xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename)
+	else:
+		xbmcgui.Dialog().ok('App Not Installed', 'This is only a shortcut.  Please install the "%s" app (%s) and try again.' % (appname, packagename))
 
 launcher()
