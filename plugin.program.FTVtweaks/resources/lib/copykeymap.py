235083a1cfa33e108e869b47e7b830f8b2635249
elmerohueso <elmerohueso@gmail.com>
2014-07-16 19:51:52 -0600

    ftvtweaks 1.1

--- plugin.program.FTVtweaks/resources/lib/copykeymap.py
+++ plugin.program.FTVtweaks/resources/lib/copykeymap.py
@@ -2,17 +2,18 @@ import shutil
 import xbmcgui
 import xbmc
 import sys
+import xbmcvfs
 
 profilepath = xbmc.translatePath('special://profile')
 resourcepath = os.getcwd()[:-4]
 
 def setkeymap(keymap):
-	shutil.copy('%s/keymaps/keymap%s.xml' % (resourcepath,keymap), '%s/keymaps/keyboard.xml' % profilepath)
+	xbmcvfs.copy('%s/keymaps/keymap%s.xml' % (resourcepath,keymap), '%s/keymaps/keyboard.xml' % profilepath)
 	xbmcgui.Dialog().ok("Complete","Keymap %s has been set." % keymap)
 
 if sys.argv[1] == 'default':
 	try:
-		os.remove('%s/keymaps/keyboard.xml' % profilepath)
+		xbmcvfs.delete('%s/keymaps/keyboard.xml' % profilepath)
 	except OSError:
 		pass
 	xbmcgui.Dialog().ok("Complete","Reverted to default keymap.")
--- plugin.program.FTVtweaks/resources/lib/copysplash.py
+++ plugin.program.FTVtweaks/resources/lib/copysplash.py
@@ -1,18 +1,19 @@
 import shutil
 import xbmcgui
 import xbmc
+import xbmcvfs
 import sys
 
 xbmchome = xbmc.translatePath('special://home')
 resourcepath = os.getcwd()[:-4]
 
 def setsplash(screen):
-	shutil.copy('%s/splash/splash%s.png' % (resourcepath,screen), '%s/media/splash.png' % xbmchome)
+	xbmcvfs.copy('%s/splash/splash%s.png' % (resourcepath,screen), '%s/media/splash.png' % xbmchome)
 	xbmcgui.Dialog().ok("Complete","Splashscreen %s has been set." % screen)
 
 if sys.argv[1] == 'default':
 	try:
-		os.remove('%s/media/splash.png' % xbmchome)
+		xbmcvfs.delete('%s/media/splash.png' % xbmchome)
 	except OSError:
 		pass
 	xbmcgui.Dialog().ok("Complete","Reverted to default splashscreen.")
