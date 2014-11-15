56acdb8d1e2f620ab1227685277f85b57b8608e1
Brendan Bone <elmerohueso@gmail.com>
2014-11-14 19:53:35 -0700

    FTVnetflix 1.4

--- script.program.FTVnetflix/addon.py
+++ script.program.FTVnetflix/addon.py
@@ -3,13 +3,17 @@ import xbmcgui
 import os
 
 appname="Netflix"
-packagename="com.netflix.ninja"
+packagename1="org.chromium.content_shell_apk"
+packagename2="com.netflix.ninja"
 
 def launcher():
-	packagepath = os.popen('pm path %s' % packagename).read()
-	if packagepath:
-		xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename)
+	packagepath1 = os.popen('pm path %s' % packagename1).read()
+	packagepath2 = os.popen('pm path %s' % packagename2).read()
+	if packagepath1:
+		xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename1)
+	elif packagepath2:
+		xbmc.executebuiltin('XBMC.StartAndroidActivity("%s")' % packagename2)
 	else:
-		xbmcgui.Dialog().ok('App Not Installed', 'This is only a shortcut.  Please install the "%s" app (%s) and try again.' % (appname, packagename))
+		xbmcgui.Dialog().ok('App Not Installed', 'This is only a shortcut.  Please install the "%s" app (%s or %s) and try again.' % (appname, packagename1, packagename2))
 
 launcher()
